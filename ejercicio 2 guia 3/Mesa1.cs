7c2ffc31f19b90d79f0821789bc94687abe763d8
leochaparro05 <126813431+leochaparro05@users.noreply.github.com>
2024-10-07 17:35:26 -0300

    Add files via upload

---
+++ ejercicio 2 guia 3/Mesa1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2_guia_3
+{
+    internal class Mesa1 : Producto
+    {
+        private double Largo;
+        public Mesa1(string descripcion, double preciobase, int tipomaterial, double largo) : base(descripcion, preciobase, tipomaterial)
+        {
+            Largo = largo;
+        }
+
+        public override double Calcularprecio()
+        {
+            double precioF = (PrecioBase * Largo) * (1 + TipoMaterial * 0.33);
+            return precioF;  
+        
+        }
+
+        string material;
+        public override string Verdetalle()
+        {
+            if (TipoMaterial == 0)
+            {
+                 material = "pino";
+            }
+            else if(TipoMaterial == 1) 
+            {
+                material = "algarrobo";
+            }
+            else 
+            {
+                material = "caoba";
+                    
+            }
+
+            return $""
+                
+        }
+
+
+
+
+    }
+}
---
+++ ejercicio 2 guia 3/Producto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2_guia_3
+{
+     abstract class Producto
+    {
+        private string Descripcion;
+        protected double PrecioBase;
+        protected int TipoMaterial;
+
+         public Producto(string descripcion, double preciobase, int tipomaterial)
+        { Descripcion = descripcion;
+            PrecioBase = preciobase;
+            TipoMaterial = tipomaterial; 
+        }
+
+        abstract public double Calcularprecio();
+        abstract public string Verdetalle();
+
+
+
+    }
+}
